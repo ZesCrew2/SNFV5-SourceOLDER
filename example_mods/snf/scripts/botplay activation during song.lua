864b767ef32556e55aa2db0647ce5b485eb6d2ea
Herox/Arie Temps <77843358+iiHeroSuper@users.noreply.github.com>
2025-06-13 22:36:22 -0300

    Actually added practice mode to this lua this time
    lily what

--- example_mods/snf/scripts/botplay activation during song.lua
+++ example_mods/snf/scripts/botplay activation during song.lua
@@ -1,10 +1,11 @@
-debugCPUControlled = true
+debugCPUControlled = true -- disable this when it releases lol
 
 firstControl = 'W'
 secondControl = 'Q'
 thirdControl = 'P'
 switchBotplayControl = true
 switchPracticeControl = true
+switchIconControl = true
 
 heroxLegacyControl = false
 
@@ -16,6 +17,44 @@ switchPracticeControl = false
 elseif keyboardJustPressed(''..thirdControl..'') and switchPracticeControl == false then
 setProperty('practiceMode', false)
 switchPracticeControl = true
+end
+
+if keyboardJustPressed('FIVE') and switchIconControl == true then
+playSound('ring', 1)
+runHaxeCode([[game.iconP1.changeIcon('funnytroll');]])
+switchIconControl = false
+elseif keyboardJustPressed('FIVE') and switchIconControl == false then
+switchIconControl = true
+	if boyfriendName == 'sonicsmol' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]])
+	
+	elseif boyfriendName == 'sonicbbt' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]])
+	
+	elseif boyfriendName == 'sonicmental' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]])
+	
+	elseif boyfriendName == 'sonicskeld' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]])
+	
+	elseif boyfriendName == 'sonicrb2' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]])
+	
+	elseif boyfriendName == 'syngoober' then
+	runHaxeCode([[game.iconP1.changeIcon('syngooberflip');]])
+	
+	-- welcome to "WHAT THE FUCK DO YOU MEAN NUH UH?!??" part (- Herox/Arie Temps)
+	--[[
+	elseif boyfriendName == 'sonic' then
+	runHaxeCode([[game.iconP1.changeIcon('sonic');]--])
+	
+	elseif boyfriendName == 'ralsei' then
+	runHaxeCode([[game.iconP1.changeIcon('ralsei');]--])
+	]]
+	
+	else
+	runHaxeCode([[game.iconP1.changeIcon(']]..string.lower(boyfriendName)..[[');]])
+	end
 end
 			
 	if debugCPUControlled == true then
