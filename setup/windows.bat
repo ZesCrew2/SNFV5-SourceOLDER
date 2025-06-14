e64607367d062e1fc8219435173af925d43b70a8
Jake Official ♪ <141687741+Jake-Official@users.noreply.github.com>
2025-06-14 15:53:29 +0100

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -9,8 +9,8 @@ haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
-haxelib install SScript 19.0.618
 haxelib install tjson 1.4.0
+haxelib git SScript https://github.com/AltronMaxX/SScript.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
